1941ec8beac5147c870a79e012e92d6acd48e740
perrygmalcolm <149367051+perrygmalcolm@users.noreply.github.com>
2023-10-30 03:21:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_xrzxufr", "template_lzc14d8", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
